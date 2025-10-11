ff9de671181f6a38e6cb70d1f5d8b717adad9abb
nextor <nextor@gmail.com>
2025-10-11 13:00:23 -0700

    Adding empty pages

---
+++ src/app/pages/save-quote/save-quote.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-save-quote',
+  imports: [],
+  templateUrl: './save-quote.component.html',
+  styleUrl: './save-quote.component.css'
+})
+export class SaveQuoteComponent {
+
+}
