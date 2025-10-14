3a0c1caa3ffd6db6af31c6642795d09fbed6c15d
nextor <nextor@gmail.com>
2025-10-14 01:12:45 -0700

    Update emptie for ecommerce

--- src/app/models/Empties/Ecommerce.ts
+++ src/app/models/Empties/Ecommerce.ts
@@ -7,5 +7,9 @@ export function ecommerce(): Ecommerce {
 		pos_id: 0, 
 		pos_session_id: null, 
 		pos_main_user_id: 0, 
+		color: '', 
+		banner_image_id: null, 
+		font_color: '', 
+		logo_image_id: null, 
 	};
 }
