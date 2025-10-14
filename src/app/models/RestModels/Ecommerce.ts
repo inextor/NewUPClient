8e0d115778696a07498b566a4313c0160e9aee81
nextor <nextor@gmail.com>
2025-10-14 01:13:06 -0700

    Updating models and empties

--- src/app/models/RestModels/Ecommerce.ts
+++ src/app/models/RestModels/Ecommerce.ts
@@ -4,6 +4,10 @@ export interface Ecommerce {
   pos_id: number;
   pos_session_id: string | null;
   pos_main_user_id: number;
+  color: string;
+  banner_image_id: number | null;
+  font_color: string;
+  logo_image_id: number | null;
 }
 
 
