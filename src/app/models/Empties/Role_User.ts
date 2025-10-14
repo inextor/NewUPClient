1b47bd2570fb70a10f3e242f3e86c6a8a378d566
nextor <nextor@gmail.com>
2025-10-14 00:53:39 -0700

    update model

--- src/app/models/Empties/Role_User.ts
+++ src/app/models/Empties/Role_User.ts
@@ -1,6 +1,6 @@
-import { User_Role } from '../RestModels/User_Role';
+import { Role_User } from '../RestModels/Role_User';
 
-export function user_role(): User_Role {
+export function role_user(): Role_User {
 	return {
 		id: 0, 
 		user_id: 0, 
