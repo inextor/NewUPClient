c226b8a9691bbaf5a24a3cc00fe30f330c4ba91e
nextor <nextor@gmail.com>
2025-10-17 14:16:21 -0700

    Adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LogoutComponent } from './pages/logout/logout.component';
 import { ListUserComponent } from './pages/list-user/list-user.component';
+import { ListRoleComponent } from './pages/list-role/list-role.component';
+import { SaveRoleComponent } from './pages/save-role/save-role.component';
 import { ListItemComponent } from './pages/list-item/list-item.component';
 import { MainComponent } from './pages/main/main.component';
 import { CartComponent } from './pages/cart/cart.component';
@@ -17,6 +19,9 @@ export const routes: Routes = [
         children: [
             { path: '', redirectTo: 'list-user', pathMatch: 'full' },
             { path: 'list-user', component: ListUserComponent },
+            { path: 'list-role', component: ListRoleComponent },
+            { path: 'add-role', component: SaveRoleComponent },
+            { path: 'edit-role/:id', component: SaveRoleComponent },
             { path: 'list-item', component: ListItemComponent },
             { path: 'cart', component: CartComponent },
             { path: 'save-quote', component: SaveQuoteComponent },
