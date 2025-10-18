24dff900021f8946f0d031f673096854538c9417
nextor <nextor@gmail.com>
2025-10-17 23:37:09 -0700

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,13 @@ import { ListRoleComponent } from './pages/list-role/list-role.component';
 import { SaveRoleComponent } from './pages/save-role/save-role.component';
 import { ListItemComponent } from './pages/list-item/list-item.component';
 import { ListRoleItemComponent } from './pages/list-role-item/list-role-item.component';
+import { ListRoleUserComponent } from './pages/list-role-user/list-role-user.component';
 import { MainComponent } from './pages/main/main.component';
 import { MainHomeComponent } from './pages/main-home/main-home.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { SaveQuoteComponent } from './pages/save-quote/save-quote.component';
 import { SaveUserComponent } from './pages/save-user/save-user.component';
+import { ImportUsersComponent } from './pages/import-users/import-users.component';
 
 import { authGuard } from './guards/auth.guard';
 
@@ -28,9 +30,11 @@ export const routes: Routes = [
             { path: 'edit-role/:id', component: SaveRoleComponent },
             { path: 'list-item', component: ListItemComponent },
             { path: 'list-role-item', component: ListRoleItemComponent },
+            { path: 'list-role-user', component: ListRoleUserComponent },
             { path: 'cart', component: CartComponent },
             { path: 'save-quote', component: SaveQuoteComponent },
             { path: 'save-user', component: SaveUserComponent },
+            { path: 'import-users', component: ImportUsersComponent },
         ]
     },
     {
