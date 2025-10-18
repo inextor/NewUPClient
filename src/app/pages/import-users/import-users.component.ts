523fe3ac75cccf3a804ea05db8be47f94afd4cf1
nextor <nextor@gmail.com>
2025-10-17 23:33:24 -0700

    feat: Add file input and import button to import-users component
    This commit enhances the ImportUsersComponent by adding:
    - A file input field for selecting a file to import.
    - An "Import" button to trigger the import process.

---
+++ src/app/pages/import-users/import-users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-import-users',
+  imports: [],
+  templateUrl: './import-users.component.html',
+  styleUrl: './import-users.component.css'
+})
+export class ImportUsersComponent {
+
+}
