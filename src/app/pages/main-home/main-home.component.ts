6d308afeab227cb09819c390b5041e4e43cb2bd7
nextor <nextor@gmail.com>
2025-10-17 20:01:59 -0700

    Adding main component

---
+++ src/app/pages/main-home/main-home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-home.component.html',
+  styleUrls: ['./main-home.component.css']
+})
+export class MainHomeComponent {
+
+}
