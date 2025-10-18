39a2dcfeabe5feb7e292b71f5f6d35dafce17b27
nextor <nextor@gmail.com>
2025-10-18 01:48:29 -0700

    feat: Refactor user save functionality
    - Updates the save-user component to handle both creating and editing users more robustly.
    - Adds validation for required fields and handles empty optional fields gracefully.
    - Improves user feedback with success and error messages.
    - Changes the edit user route to /edit-user for clarity.
    - Standardizes the user initialization using GetEmpty.user().

--- src/app/pages/save-user/save-user.component.ts
+++ src/app/pages/save-user/save-user.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Rest } from '../../classes/Rest';
-import { BaseComponent, ParamsAndQueriesMap } from '../base/base.component';
+import { BaseComponent } from '../base/base.component';
 import { GetEmpty } from '../../models/GetEmpty';
 
 @Component({
@@ -17,48 +17,54 @@ import { GetEmpty } from '../../models/GetEmpty';
 })
 export class SaveUserComponent extends BaseComponent implements OnInit {
 
-	rest_user = new Rest<User,User>(this.rest, 'user');
-
-	user: User = {
-			id: 0,
-			name: '',
-			username: '',
-			password: '',
-			type: 'USER',
-			ecommerce_id: 0,
-			created: '',
-			updated: '',
-			code: null
-	};
+	rest_user: Rest<User, User> = new Rest<User, User>(this.rest, 'user.php');
+	user: User = GetEmpty.user();
 
 	ngOnInit(): void {
-		this.getParamsAndQueriesObservable().subscribe
-		({
-			next: (response: ParamsAndQueriesMap) => {
-
-				let p = response.query.has('id')
-					? this.rest_user.get(response.query.get('id') as string)
-					: Promise.resolve(GetEmpty.user());
-
-				p.then(response => {
-					this.user = response;
-				});
-			}
+		this.route.queryParamMap.subscribe(params => {
+			const id = params.get('id');
+			let p = id ? this.rest_user.get(id) : Promise.resolve(GetEmpty.user());
+			p.then((user) => {
+				this.user = user;
+			})
+			.catch((err) => {
+				this.rest.showError(err);
+			});
 		});
 	}
 
-	saveUser() {
-		this.is_loading = true;
-		if (this.user.id) {
-			this.rest_user.create(this.user).then(response => {
-				this.is_loading = false;
-				this.router.navigate(['/list-user']);
-			});
-		} else {
-			this.rest_user.update(this.user).then(response => {
-				this.is_loading = false;
-				this.router.navigate(['/list-user']);
-			});
+	save() {
+		// Validate only name is required
+		if (!this.user.name || this.user.name.trim() === '') {
+			this.rest.showError('El nombre es requerido');
+			return;
 		}
+
+		// Set username to null if empty
+		if (!this.user.username || this.user.username.trim() === '') {
+			this.user.username = null as any;
+		}
+
+		// Set code to null if empty
+		if (!this.user.code || this.user.code.trim() === '') {
+			this.user.code = null;
+		}
+
+		// Set password to null if empty
+		if (!this.user.password || this.user.password.trim() === '') {
+			this.user.password = null;
+		}
+
+		this.is_loading = true;
+		let promise = this.user.id ? this.rest_user.update(this.user) : this.rest_user.create(this.user);
+		promise.then(() => {
+			this.is_loading = false;
+			this.rest.showSuccess('Usuario guardado correctamente');
+			this.router.navigate(['/list-user']);
+		})
+		.catch((err) => {
+			this.is_loading = false;
+			this.rest.showError(err);
+		});
 	}
 }
