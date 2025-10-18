e068727f5b2e64a4872bd351a19e2a2519608808
nextor <nextor@gmail.com>
2025-10-17 19:42:28 -0700

    FIX ALL

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,17 +3,22 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { RestService } from '../../services/rest.service';
 import { CommonModule } from '@angular/common';
 
+import { HeaderComponent } from '../../components/header/header.component';
+
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+	selector: 'app-main',
+	standalone: true,
+	imports: [RouterOutlet, RouterLink, CommonModule, HeaderComponent],
+	templateUrl: './main.component.html',
+	styleUrl: './main.component.css'
 })
 export class MainComponent {
-  rest = inject(RestService);
+	constructor(public rest: RestService)
+	{
 
-  toggleMenu() {
-    this.rest.toggleMenu();
-  }
+	}
+
+	toggleMenu() {
+		this.rest.toggleMenu();
+	}
 }
