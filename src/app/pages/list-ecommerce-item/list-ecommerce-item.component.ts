f4b7fd65926ca6714a5c6d87453f32fa9fec08f9
nextor <nextor@gmail.com>
2025-10-21 23:15:21 -0700

    Adding search

--- src/app/pages/list-ecommerce-item/list-ecommerce-item.component.ts
+++ src/app/pages/list-ecommerce-item/list-ecommerce-item.component.ts
@@ -6,6 +6,7 @@ import { SearchObject } from '../../classes/SearchObject';
 import { ParamMap, RouterLink } from '@angular/router';
 import { RestResponse } from '../../classes/RestResponse';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ImagePipe } from '../../pipes/image.pipe';
 import { PaginationComponent } from '../../components/pagination/pagination.component';
 
@@ -16,7 +17,7 @@ interface CEcommerceItemInfo {
 
 @Component({
 	selector: 'app-list-ecommerce-item',
-	imports: [CommonModule, ImagePipe, RouterLink, PaginationComponent],
+	imports: [CommonModule, FormsModule, ImagePipe, RouterLink, PaginationComponent],
 	templateUrl: './list-ecommerce-item.component.html',
 	styleUrl: './list-ecommerce-item.component.css'
 })
@@ -26,6 +27,7 @@ export class ListEcommerceItemComponent extends BaseComponent implements OnInit 
 	rest_ecommerce_item: Rest<Ecommerce_Item,Ecommerce_Item> = new Rest<Ecommerce_Item,Ecommerce_Item>(this.rest, 'ecommerce_item.php');
 	ecommerce_item_list: Ecommerce_Item[] = [];
 	cecommerce_item_info: CEcommerceItemInfo[] = [];
+	searchTerm: string = '';
 
 	ngOnInit(): void
 	{
@@ -39,6 +41,11 @@ export class ListEcommerceItemComponent extends BaseComponent implements OnInit 
 			this.current_page = page;
 			this.page_size = limit;
 
+			// Get search term from query params if exists
+			if (params.has('name~~')) {
+				this.searchTerm = params.get('name~~')!;
+			}
+
 			let url_params = this.rest_ecommerce_item.getUrlParams( params );
 
 			//url_params.set('ecommerce_id', ''+this.rest.ecommerce.id);
@@ -49,22 +56,40 @@ export class ListEcommerceItemComponent extends BaseComponent implements OnInit 
 				this.ecommerce_item_list = response.data;
 				this.setPages(this.current_page, response.total);
 				let ids = response.data.map((i)=>i.item_id);
-				return Promise.all([response, this.rest_item.search({ 'id,': ids })]);
+
+				// Build search params for items
+				let item_search_params: any = { 'id,': ids };
+
+				// If there's a search term, add it to filter by name
+				if (this.searchTerm) {
+					item_search_params['name~~'] = this.searchTerm;
+				}
+
+				return Promise.all([response, this.rest_item.search(item_search_params)]);
 			})
 			.then(([response, item_response]) =>
 			{
-				this.cecommerce_item_info = response.data.map((item:Ecommerce_Item, i:number) =>
-				{
-					return {
-						ecommerce_item: item,
-						item_info: item_response.data[i]
-					}
-				});
+				// Filter ecommerce items to only include those with matching item_info
+				const matched_item_ids = new Set(item_response.data.map((item: any) => item.item.id));
+
+				this.cecommerce_item_info = response.data
+					.filter((ecommerce_item: Ecommerce_Item) => matched_item_ids.has(ecommerce_item.item_id))
+					.map((ecommerce_item: Ecommerce_Item) => {
+						const item_info = item_response.data.find((item: any) => item.item.id === ecommerce_item.item_id);
+						return {
+							ecommerce_item: ecommerce_item,
+							item_info: item_info
+						}
+					});
 			})
 			.catch((error:any) =>
 			{
 				this.rest.showError(error);
 			});
 		});
+	}
+
+	onSearch(): void {
+		this.router.navigate(['/list-ecommerce-item'], { queryParams: { 'name~~': this.searchTerm, 'page': 0 } });
 	}
 }
